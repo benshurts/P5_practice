c749c144761e3c0fc3613c4857d8b0df12c9abeb
Ben Shurts <benjaminshurts@gmail.com>
2019-01-31 13:41:00 -0700

    set up

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,7 +1,10 @@
 function setup() {
-  // put setup code here
+    // put setup code here
+
 }
 
 function draw() {
-  // put drawing code here
+    // put drawing code here
+    ellipse(56, 46, 55, 55);
+
 }
